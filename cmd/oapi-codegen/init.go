2657e559069a31d01d0206884c5f772b03fcf967
Tiago Angelo <tiago.angelo@totvs.com.br>
2026-09-25 15:56:14 -0300

    Add JSON request body template functions

--- cmd/oapi-codegen/init.go
+++ cmd/oapi-codegen/init.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	sprig "github.com/Masterminds/sprig/v3"
 	"github.com/oapi-codegen/oapi-codegen/v2/pkg/codegen"
 )
@@ -11,4 +13,40 @@ func init() {
 			codegen.TemplateFunctions[name] = fn
 		}
 	}
+
+	codegen.TemplateFunctions["jsonRequestBody"] = jsonRequestBody
+	codegen.TemplateFunctions["genJSONRequestBodyArg"] = genJSONRequestBodyArg
+}
+
+func jsonRequestBody(op *codegen.OperationDefinition) *codegen.RequestBodyDefinition {
+	var body *codegen.RequestBodyDefinition
+
+	for i := range op.Bodies {
+		candidate := &op.Bodies[i]
+
+		if !candidate.IsJSON() {
+			continue
+		}
+
+		if candidate.Default {
+			return candidate
+		}
+
+		if body == nil {
+			body = candidate
+		}
+	}
+
+	return body
+}
+
+func genJSONRequestBodyArg(op *codegen.OperationDefinition) string {
+	body := jsonRequestBody(op)
+	if body == nil {
+		return ""
+	}
+
+	typeName := body.TypeDef(op.OperationId).TypeName
+
+	return fmt.Sprintf(", body *%s", typeName)
 }
